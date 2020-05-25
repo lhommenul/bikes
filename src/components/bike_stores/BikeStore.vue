552ea0071e6a20f5696fc56514cf3e6c3647a2bf
lhommenul <tdtwiste@gmail.com>
2020-05-25 15:33:45 +0200

    updatepage

--- src/components/bike_stores/BikeStore.vue
+++ src/components/bike_stores/BikeStore.vue
@@ -1,7 +1,16 @@
 <template>
   <div :class="{bikes_stores_large : posi==true, bikes_stores_small : posi == false}" :style="{'background-image':'url(https://static4.pagesjaunes.fr/media/cviv/06400781-c595e66ada1b9022f7ab30cb008cdd30)'}">
-    <h1>Nom Moto Ecole</h1>
-    <h4>rating stars</h4>
+    <div>
+      <h1>Nom Moto Ecole</h1>
+      <h4>rating stars</h4>
+    </div>
+    <div>
+      <ul>
+        <li><button class="lesson_given">Moto</button></li>
+        <li><button class="lesson_given">Voiture</button></li>
+        <li><button class="lesson_given">Poids Lourd</button></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -35,5 +44,14 @@ background-size: cover;
     background-position: center;
     background-size: cover;
 }
-
+.lesson_given{
+  cursor: pointer;
+  margin: 5px;
+  padding: 5px;
+  border-radius: 3px;
+  font-size: 16px;
+  background-color: rgb(43, 87, 117);
+  color: white;
+  border-style: none;
+}
 </style>
